98bb988cc4dd4e6c14339aed41a5ba281a01ab0f
Daniil Ratakhin <ratahind@mail.ru>
2021-03-04 18:53:16 +0700

    second try

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from "./abc";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <Hello />
         </a>
       </header>
     </div>
